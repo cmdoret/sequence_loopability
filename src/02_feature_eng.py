25c400f6dcb2be2e24816dd140e4b60765bcb76d
cmdoret <cyril.mattheydoret@gmail.com>
2021-03-28 03:15:50 +0200

    create script for feature engineering

---
+++ src/02_feature_eng.py
@@ -0,0 +1,9 @@
+# Transforming and standardizing features to analyze later on.
+
+# Basic sequence stats, GC, entropy, K-mer divergence, ...
+
+# Summary stats of histone cov
+
+# Tokenizationi and dna2vec training ?
+
+# 2 outputs: embedded sequence and extracted features
