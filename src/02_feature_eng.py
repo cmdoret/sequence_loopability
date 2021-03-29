24841cddcba0abd0fa671bc9a0582332d721d13b
cmdoret <cyril.mattheydoret@gmail.com>
2021-03-29 03:47:14 +0200

    placeholder script for feature engineering

--- src/02_feature_eng.py
+++ src/02_feature_eng.py
@@ -1,9 +1,14 @@
+import sys
+import pandas as pd
+
+GENOME = sys.argv[1]
+
+# Encode input sequences with precomputed embedding
+
 # Transforming and standardizing features to analyze later on.
 
 # Basic sequence stats, GC, entropy, K-mer divergence, ...
 
 # Summary stats of histone cov
-
-# Tokenizationi and dna2vec training ?
 
 # 2 outputs: embedded sequence and extracted features
